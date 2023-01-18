7c1bc7ff873e7da3363a6b31ff25491443af574b
Bernardo Restrepo <bernardorestrepo@veryfi.com>
2023-01-18 11:31:37 -0500

    Improve Headless receipts demo
    Add Headless receipts flavor
    Add logs activity
    Improve theme
    Add material icons
    Update README

---
+++ app/build/generated/source/buildConfig/debug/com/veryfi/lens/headless/receipts/demo/BuildConfig.java
@@ -0,0 +1,20 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.veryfi.lens.headless.receipts.demo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.veryfi.lens.headless.receipts.demo";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String VERYFI_API_KEY = "1171431d8e6eb8478a50d872bcb2dc51";
+  // Field from default config.
+  public static final String VERYFI_CLIENT_ID = "vrfdR0ALCMGfx32RO0gMFyfZeJT7UCliNxZZLnG";
+  // Field from default config.
+  public static final String VERYFI_URL = "https://api.veryfi.com/";
+  // Field from default config.
+  public static final String VERYFI_USERNAME = "devapitest";
+}
